1ff2466b96e8ea3f143571854381390b39bd3f3a
govind <govind.g.coder@gmail.com>
2026-02-20 05:24:55 +0000

    removed ../

--- src/FileCopy.java
+++ src/FileCopy.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class FileCopy {
     public static void main(String[] args) {
-        String sourceFile = "../source.txt";
+        String sourceFile = "source.txt";
         String destFile = "destination.txt";
 
         // Part 1: Copy from source to destination using Byte Streams
